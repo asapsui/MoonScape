c4bed2d13c88c958014d771b5c73c0f4810bab53
Sam Imose <samuel.u.imose@gmail.com>
2022-05-10 13:46:55 -0500

    Update Player.java
    Added hashtable of textures and health properties for the player

--- MoonScape/src/entity/Player.java
+++ MoonScape/src/entity/Player.java
@@ -1,34 +1,52 @@
-package entity;
+package main;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import main.Game;
+import java.util.Hashtable;
 
 public class Player  
 implements KeyListener {
 	
-	// made x and y public so its accessible to the ray class
-	private Camera cam;
-	public double x;
-    public double y;
+	double x;
+    double y;
 	
     int turnDirection; // -1 for left, +1 for right
     int walkDirection; // -1 for walking backwards(up),// +1 for walking forwards(down)
     public double rotationAngle;
     public double moveSpeed;
     public double rotationSpeed;
+    public int health;
+    public int maxHealth;
+    public static Texture currentHealthImage;
+    
+	public static Hashtable<Integer, Texture> HealthTable = new Hashtable<>(){{
+		put(8, new Texture("res/healthbar/8.png", 300));
+		put(7, new Texture("res/healthbar/7.png", 300));
+		put(6, new Texture("res/healthbar/6.png", 300));
+		put(5, new Texture("res/healthbar/5.png", 300));
+		put(4, new Texture("res/healthbar/4.png", 300));
+		put(3, new Texture("res/healthbar/3.png", 300));
+		put(3, new Texture("res/healthbar/2.png", 300));
+		put(1, new Texture("res/healthbar/1.png", 300));
+		put(0, new Texture("res/healthbar/0.png", 300));
 
+
+	}};
+ 
     public Player(){
     	
-        this.x = 300;
-        this.y = 250;
+        this.x = 200;
+        this.y = 800;
 
-	//try to make the cam
-	this.cam = cam;
         this.turnDirection = 0; // -1 if left, +1 if right
         this.walkDirection = 0; // -1 if back, +1 if front
         this.rotationAngle = Math.PI / 2;
         this.moveSpeed = 2;
         this.rotationSpeed = 2 * (Math.PI / 180);
+        // loads in with max health
+        this.health = 9;
+        this.maxHealth = 9;
+        currentHealthImage = HealthTable.get(8);
     }
     public void update(int[][]map){
         this.rotationAngle += this.turnDirection * this.rotationSpeed;
@@ -40,6 +58,21 @@ implements KeyListener {
             this.x = newPlayerX;
             this.y = newPlayerY;
         }
+        
+        
+        // since we are updating the players position 60 times per second
+        // this may mess up, since we updating the position so many times a second
+        if (health != 0 && maxHealth != 0) {
+	        if (health < maxHealth) {
+	        	maxHealth = health;
+	        	// change the image for the health bar
+	        	currentHealthImage = HealthTable.get(health);
+	        }
+        }
+        else {
+        	currentHealthImage = HealthTable.get(0);
+        }
+        
     }
 
     public double getX(){
@@ -55,64 +88,20 @@ implements KeyListener {
     public double vectorY(){
         return this.y+ Math.sin(this.rotationAngle) * 30;
     }
+    
+    public int getHealth() {
+    	return this.health;
+    }
+    
+    public void drawHealthbar(Graphics g) {
+        g.drawImage(currentHealthImage.getImage(), 10, 800, null);
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
 
     }
 
-	//update movement (fixing)
-	/*
-	public void update(Player player, double time) {
-        if(cam.getBounds().collides(this.bounds)) {
-	
-	//collides(boolean)****
-            super.update(time);
-            chase(player);
-            move();
-
-            if(teleported) {
-                teleported = false;
-
-                bounds.setWidth(size / 2);
-                bounds.setHeight(size / 2 - yOffset);
-                bounds.setXOffset(size / 2 - xOffset);
-                bounds.setYOffset(size / 2 + yOffset);
-
-                hitBounds = new AABB(pos, size, size);
-                hitBounds.setXOffset(size / 2);
-
-                sense = new AABB(new Vector2f(pos.x + size / 2 - r_sense / 2, pos.y + size / 2 - r_sense / 2), r_sense);
-                attackrange = new AABB(new Vector2f(pos.x + bounds.getXOffset() + bounds.getWidth() / 2 - r_attackrange / 2 , pos.y + bounds.getYOffset() + bounds.getHeight() / 2 - r_attackrange / 2 ), r_attackrange);
-            }
-
-            if(attackrange.colCircleBox(player.getBounds()) && !isInvincible) {
-                attack = true;
-                player.setHealth(player.getHealth() - damage, 5f * getDirection(), currentDirection == UP || currentDirection == DOWN);
-            } else {
-                attack = false;
-            }
-
-            if (!fallen) {
-                if (!tc.collisionTile(dx, 0)) {
-                    sense.getPos().x += dx;
-                    attackrange.getPos().x += dx;
-                    pos.x += dx;
-                }
-                if (!tc.collisionTile(0, dy)) {
-                    sense.getPos().y += dy;
-                    attackrange.getPos().y += dy;
-                    pos.y += dy;
-                }
-            } else {
-                if(ani.hasPlayedOnce()) {
-                    die = true;
-                }
-            }
-        }
-    }
-	*/
-	
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP){
