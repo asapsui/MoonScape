2916f3bec9bb0a94031aaf7568b3f36fea494700
MarilynBeth <89665740+MarilynBeth@users.noreply.github.com>
2022-05-12 21:13:10 -0500

    Update Player.java

--- MoonScape/src/Player.java
+++ MoonScape/src/Player.java
@@ -1,25 +1,50 @@
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Hashtable;
 
-public class Player  implements KeyListener {
-    double x;
+public class Player
+implements KeyListener {
+
+	double x;
     double y;
+
     int turnDirection; // -1 for left, +1 for right
     int walkDirection; // -1 for walking backwards(up),// +1 for walking forwards(down)
-    double rotationAngle;
-    double moveSpeed;
-    double rotationSpeed;
+    public double rotationAngle;
+    public double moveSpeed;
+    public double rotationSpeed;
+    public int health;
+    public int maxHealth;
+    public static Texture currentHealthImage;
+
+	public static Hashtable<Integer, Texture> HealthTable = new Hashtable<>(){{
+		put(8, new Texture("res/healthbar/8.png", 300));
+		put(7, new Texture("res/healthbar/7.png", 300));
+		put(6, new Texture("res/healthbar/6.png", 300));
+		put(5, new Texture("res/healthbar/5.png", 300));
+		put(4, new Texture("res/healthbar/4.png", 300));
+		put(3, new Texture("res/healthbar/3.png", 300));
+		put(2, new Texture("res/healthbar/2.png", 300));
+		put(1, new Texture("res/healthbar/1.png", 300));
+		put(0, new Texture("res/healthbar/0.png", 300));
+	}};
 
     public Player(){
-
-        this.x = 300;
-        this.y = 250;
+        this.x = 200;
+        this.y = 800;
 
         this.turnDirection = 0; // -1 if left, +1 if right
         this.walkDirection = 0; // -1 if back, +1 if front
-        this.rotationAngle = Math.PI / 2;
+
+        this.rotationAngle = 270 * (Math.PI/180);
+
         this.moveSpeed = 2;
-        this.rotationSpeed = 1.5 * (Math.PI / 180);
+        this.rotationSpeed = 2 * (Math.PI / 180);
+        // loads in with max health
+        this.health = 8;
+        this.maxHealth = 8;
+        currentHealthImage = HealthTable.get(8);
     }
     public void update(int[][]map){
         this.rotationAngle += this.turnDirection * this.rotationSpeed;
@@ -32,6 +57,21 @@ public class Player  implements KeyListener {
             this.x = newPlayerX;
             this.y = newPlayerY;
         }
+
+
+        // since we are updating the players position 60 times per second
+        if (health != 0 && maxHealth != 0) {
+	        if (health < maxHealth) {
+	        	maxHealth = health;
+	        	// change the image for the health bar
+	        	System.out.println("Max Health: " + maxHealth );
+	        	currentHealthImage = HealthTable.get(health);
+	        }
+        }
+        else {
+        	currentHealthImage = HealthTable.get(0);
+        }
+
     }
 
     public double getX(){
@@ -42,13 +82,15 @@ public class Player  implements KeyListener {
         return this.y;
     }
     public double vectorX(){
-        return this.x+ Math.cos(this.rotationAngle) * 30;
+        return this.x + Math.cos(this.rotationAngle) * 30;
     }
     public double vectorY(){
         return this.y+ Math.sin(this.rotationAngle) * 30;
     }
 
-
+    public void drawHealthbar(Graphics g) {
+        g.drawImage(currentHealthImage.getImage(), 10, 800, null);
+    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -57,32 +99,32 @@ public class Player  implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyChar() == 'w'){
+        if(e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP){
             this.walkDirection = +1;
         }
-        if(e.getKeyChar()=='s'){
+        if(e.getKeyChar()=='s' || e.getKeyCode() == KeyEvent.VK_DOWN){
             this.walkDirection =-1;
         }
-        if(e.getKeyChar() == 'd'){
+        if(e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT){
             this.turnDirection = +1;
         }
-        if(e.getKeyChar() == 'a'){
+        if(e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT){
             this.turnDirection = -1;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if(e.getKeyChar() == 'w'){
+        if(e.getKeyChar() == 'w' || e.getKeyCode() == KeyEvent.VK_UP){
             this.walkDirection = 0;
         }
-        if(e.getKeyChar()=='s'){
-            this.walkDirection =0;
+        if(e.getKeyChar()=='s' || e.getKeyCode() == KeyEvent.VK_DOWN){
+            this.walkDirection = 0;
         }
-        if(e.getKeyChar() == 'd'){
+        if(e.getKeyChar() == 'd' || e.getKeyCode() == KeyEvent.VK_RIGHT){
             this.turnDirection = 0;
         }
-        if(e.getKeyChar() == 'a'){
+        if(e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT){
             this.turnDirection = 0;
         }
     }
