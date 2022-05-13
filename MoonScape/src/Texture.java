84e970a66b3513368dec0ef6d3f584a4800171fe
MarilynBeth <89665740+MarilynBeth@users.noreply.github.com>
2022-05-12 21:14:46 -0500

    Update Texture.java

--- MoonScape/src/Texture.java
+++ MoonScape/src/Texture.java
@@ -1,13 +1,17 @@
 import javax.imageio.ImageIO;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Hashtable;
 
 public class Texture {
+	
     public int[] pixels;
-    private String location;
+    public String location;
     public final int SIZE;
-
+    protected BufferedImage image;
+    
     public Texture(String location, int size){
         this.location = location;
         this.SIZE = size;
@@ -17,7 +21,7 @@ public class Texture {
     }
     private void load(){
         try{
-            BufferedImage image = ImageIO.read(new File(location));
+            image = ImageIO.read(new File(location));
             int w = image.getWidth();
             int h = image.getHeight();
             image.getRGB(0,0,w,h,pixels,0,w);
@@ -28,10 +32,24 @@ public class Texture {
             System.out.println("sad days");
         }
     }
-    public static Texture wood = new Texture("src/res/wood.png", 64);
-    public static Texture brick = new Texture("src/res/redbrick.png", 64);
-    public static Texture bluestone = new Texture("src/res/bluestone.png", 64);
-    public static Texture stone = new Texture("src/res/greystone.png", 64);
-    public static Texture moon = new Texture("src/res/moon.png", 64);
+    
+    // only the mob class is using this method and the size for all of them is 64
+    public void changeTexture(Texture t) {
+    	this.location = t.location;
+    	// reload the updated image
+    	load();  
+    }
+    
+    public Image getImage() {
+    	return image;
+    }
+       
+    public static Texture wood = new Texture("res/wood.png", 64);
+    public static Texture brick = new Texture("res/redbrick.png", 64);
+    public static Texture bluestone = new Texture("res/bluestone.png", 64);
+    public static Texture stone = new Texture("res/greystone.png", 64);
+    public static Texture moon = new Texture("res/moon.png", 64);
+    public static Texture door = new Texture("res/door.png", 64);
+    public static Texture doorWall = new Texture("res/doorWall.png", 64);
 
 }
