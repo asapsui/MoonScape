d4be13d3860c67ec5de06ae2a19d73682a951264
MarilynBeth <89665740+MarilynBeth@users.noreply.github.com>
2022-05-14 12:39:12 -0500

    Update Player.java

--- MoonScape/src/Player.java
+++ MoonScape/src/Player.java
@@ -1,4 +1,4 @@
-package main;
+
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,32 +18,41 @@ implements KeyListener {
     public int health;
     public int maxHealth;
     public static Texture currentHealthImage;
+    boolean hasKey = false;
     
 	public static Hashtable<Integer, Texture> HealthTable = new Hashtable<>(){{
 		put(8, new Texture("res/healthbar/8.png", 300));
-		put(7, new Texture("res/healthbar/7.png", 300));
-		put(6, new Texture("res/healthbar/6.png", 300));
-		put(5, new Texture("res/healthbar/5.png", 300));
-		put(4, new Texture("res/healthbar/4.png", 300));
-		put(3, new Texture("res/healthbar/3.png", 300));
-		put(2, new Texture("res/healthbar/2.png", 300));
-		put(1, new Texture("res/healthbar/1.png", 300));
-		put(0, new Texture("res/healthbar/0.png", 300));
+        //put(15, new Texture("res/healthbar/8.png", 300));
+        put(7, new Texture("res/healthbar/7.png", 300));
+		//put(13, new Texture("res/healthbar/7.png", 300));
+        put(6, new Texture("res/healthbar/6.png", 300));
+		//put(11, new Texture("res/healthbar/6.png", 300));
+        put(5, new Texture("res/healthbar/5.png", 300));
+		//put(9, new Texture("res/healthbar/5.png", 300));
+        put(4, new Texture("res/healthbar/4.png", 300));
+		//put(7, new Texture("res/healthbar/4.png", 300));
+        put(3, new Texture("res/healthbar/3.png", 300));
+		//put(5, new Texture("res/healthbar/3.png", 300));
+        put(2, new Texture("res/healthbar/2.png", 300));
+		//put(3, new Texture("res/healthbar/2.png", 300));
+        put(1, new Texture("res/healthbar/1.png", 300));
+		//put(1, new Texture("res/healthbar/1.png", 300));
+        put(0, new Texture("res/healthbar/0.png", 300));
 	}};
  
     public Player(){
-        this.x = 200;
-        this.y = 800;
+        this.x = 80;
+        this.y = 880;
 
         this.turnDirection = 0; // -1 if left, +1 if right
         this.walkDirection = 0; // -1 if back, +1 if front
         this.rotationAngle = 270 * (Math.PI/180);
-        this.moveSpeed = 2;
-        this.rotationSpeed = 2 * (Math.PI / 180);
+        this.moveSpeed = 6;
+        this.rotationSpeed = 3.5 * (Math.PI / 180);
         // loads in with max health
-        this.health = 8;
-        this.maxHealth = 8;
-        currentHealthImage = HealthTable.get(8);
+        this.health = 24;
+        this.maxHealth = 24;
+        currentHealthImage = HealthTable.get(maxHealth/3);
     }
     public void update(int[][]map){
         this.rotationAngle += this.turnDirection * this.rotationSpeed;
@@ -55,22 +64,27 @@ implements KeyListener {
         if (!Game.hasWallAt(newPlayerX, newPlayerY)) {
             this.x = newPlayerX;
             this.y = newPlayerY;
+
         }
         
         
         // since we are updating the players position 60 times per second
         if (health != 0 && maxHealth != 0) {
-	        if (health < maxHealth) {
+	        if (health == maxHealth - 3) {
 	        	maxHealth = health;
 	        	// change the image for the health bar
-	        	System.out.println("Max Health: " + maxHealth );
-	        	currentHealthImage = HealthTable.get(health);
+
+	        	currentHealthImage = HealthTable.get(health/3);
 	        }
         }
         else {
         	currentHealthImage = HealthTable.get(0);
         }
-        
+        if(Math.abs(this.x - Sprite.bracelet.xPosition) <=  50 && Math.abs(this.y - Sprite.bracelet.yPosition) <= 50){
+            this.hasKey = true;
+            Sprite.bracelet.Remove();
+        }
+
     }
 
     public double getX(){
@@ -109,6 +123,19 @@ implements KeyListener {
         }
         if(e.getKeyChar() == 'a' || e.getKeyCode() == KeyEvent.VK_LEFT){
             this.turnDirection = -1;
+        }
+
+
+        if(e.getKeyChar() == 'e' || e.getKeyChar() == 'E'){
+            if(Game.mapIndexAt(this.vectorX(),this.vectorY() )==7  ){
+                Game.map[Game.getYMapIndex(this.vectorY())][Game.getXMapIndex(this.vectorX())] = 77;
+            }
+            else if(Game.mapIndexAt(this.vectorX(),this.vectorY() )==77  ){
+                Game.map[Game.getYMapIndex(this.vectorY())][Game.getXMapIndex(this.vectorX())] = 7;
+            }
+            if(Game.mapIndexAt(this.vectorX(),this.vectorY() )== 16 && this.hasKey ){
+                Game.map[Game.getYMapIndex(this.vectorY())][Game.getXMapIndex(this.vectorX())] = 0;
+            }
         }
     }
 
